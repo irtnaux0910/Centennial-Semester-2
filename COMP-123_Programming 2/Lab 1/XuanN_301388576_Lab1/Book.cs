4a410804355a8613e445df60853d79df414c88cf
Xuan Tri Nguyen <143986810+irtnaux0910@users.noreply.github.com>
2024-08-17 01:24:03 -0400

    Add files via upload

---
+++ COMP-123_Programming 2/Lab 1/XuanN_301388576_Lab1/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XuanN_301388576_Lab1
+{
+    class Book
+    {
+        private int publicationYear;
+    private string author;
+    private string title;
+    private bool isHardCover;
+    private double price;
+    public Book(int publicationYear, string author, string title, double price, bool isHardCover = false)
+    {
+        this.publicationYear = publicationYear;
+        this.author = author;
+        this.title = title;
+        this.isHardCover = isHardCover;
+        this.price = price;
+    }
+    public override string ToString()
+    {
+        string coverType;
+        if (isHardCover)
+        {
+            coverType = "hardcover";
+        }
+        else
+        {
+            coverType = "papercover";
+        }
+
+        return $"{title} is written by {author}, published in {publicationYear}. Its price is {price} and its cover type is {coverType}";
+    }
+}
+}
---
+++ COMP-123_Programming 2/Lab 1/XuanN_301388576_Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+/*Xuan Tri Nguyen - 301388576*/
+
+namespace XuanN_301388576_Lab1
+{    
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book(1813, "Jane Austen", "Pride and Prejudice", 24.99, true);
+            Book book2 = new Book(1967,"Gabriel García Márquez", "One Hundred Years of Solitude", 37);
+            Book book3 = new Book(1615, "Miguel de Cervantes", "Don Quixote",  22.99);
+            Book book4 = new Book(1925, "F. Scott Fitzgerald",  "The Great Gatsby",  20.76);
+            Book book5 = new Book(1922, "James Joyce", "Ulysses",  26.99);
+            Console.WriteLine(book1);
+            Console.WriteLine(book2);
+            Console.WriteLine(book3);
+            Console.WriteLine(book4);
+            Console.WriteLine(book5);                            
+        }
+    }
+}
