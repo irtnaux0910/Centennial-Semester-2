e8a2592e5a6db5baca50cefade110729b7562dc1
Xuan Tri Nguyen <143986810+irtnaux0910@users.noreply.github.com>
2024-08-17 01:32:47 -0400

    Add files via upload

---
+++ COMP-123_Programming 2/Final Exam/Delegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        public delegate string TextOperation(string text);        
+        static void Main(string[] args)
+        {
+            // Create delegate instances pointing to the methods
+            TextOperation uppercaseDelegate = TextProcessor.Uppercase;
+            TextOperation reverseDelegate = TextProcessor.Reverse;
+            TextOperation removeSpacesDelegate = TextProcessor.RemoveSpaces;
+
+            string inputText = "Hello, World!";
+
+            // Perform text processing using the delegates
+            string resultUppercase = uppercaseDelegate(inputText);
+            Console.WriteLine($"Uppercase result: {resultUppercase}");
+
+            string resultReverse = reverseDelegate(inputText);
+            Console.WriteLine($"Reverse result: {resultReverse}");
+
+            string resultRemoveSpaces = removeSpacesDelegate(inputText);
+            Console.WriteLine($"Remove spaces result: {resultRemoveSpaces}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ COMP-123_Programming 2/Final Exam/Delegate/TextProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class TextProcessor
+    {
+        public static string Uppercase(string text)
+        {
+            return text.ToUpper();
+        }
+
+        public static string Reverse(string text)
+        {
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            string reversed = new string(charArray);            
+            return reversed;
+        }
+        public static string RemoveSpaces(string text)
+        {
+            return text.Replace(" ","");
+        }
+    }
+}
